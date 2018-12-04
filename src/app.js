6a5a9f2a8f3e73f1465ce4e4f2c4d6a2569bd838
Tiago Lima <tiago.lima@xpi.com.br>
2018-12-04 21:42:49 -0200

    Subindo projeto

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+const App = () => (
+  <div className="app">
+    <div className="user-info">
+      <img src="https://avatars3.githubusercontent.com/u/4465962?v=4" />
+    </div>
+  </div>
+)
+
+export default App
